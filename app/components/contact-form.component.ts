88128544038c51a8fe301d098a59b977952d75f1
Barmaley86 <vk_31@mail.ru>
2016-11-15 22:26:43 +0300

    add valid form

--- app/components/contact-form.component.ts
+++ app/components/contact-form.component.ts
@@ -6,27 +6,26 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
     moduleId: module.id,
     selector: 'contact-form',
     template: `
-        <form class="ui form" (ngSubmit)="onSubmit(myForm.value)" [formGroup]="myForm">
+        <form class="ui form segment" [ngClass]="{'error' : !myForm.valid}" (ngSubmit)="onSubmit()" [formGroup]="myForm">
             <h4 class="ui dividing header">Contact Manager filer</h4>
                 
             <div class="two fields">
                 <div class="field" [ngClass]="{'error' : !myForm.controls['maxResults'].valid}">
                     <label>Name</label>
                     <input name="maxResults" placeholder="Max results" type="text" [formControl]="myForm.controls['maxResults']">
-                    <div *ngIf="!myForm.controls['maxResults'].valid" class="ui error message">maxResults is invalid</div>
                 </div>
                 <div class="field">
                     <label>Page</label>
                     <input name="page" placeholder="Page" type="text">
                 </div>
             </div>
-            <button type="submit" class="ui submit button" [disabled]="submitted">Submit</button>
-            
-        </form> 
-        <div *ngIf="!myForm.valid" class="ui ignored warning message">
-            <p>If you are looking for validation you should check out.</p>
-        </div>
-    `
+            <button type="submit" class="ui primary submit button" [disabled]="submitted">Submit</button>
+
+            <div *ngIf="!myForm.valid" class="ui error message">
+                <ul>If you are looking for validation you should check out.</ul>
+            </div>
+
+        </form>    `
 })
 
 export class ContactFormComponent {
@@ -43,8 +42,8 @@ export class ContactFormComponent {
         this.maxResults = this.myForm.controls['maxResults'];
     }
 
-    onSubmit(value: string): void {  
-        this.formResults.emit(value['maxResults']);
+    onSubmit(): void {  
+        this.formResults.emit(this.myForm.controls['maxResults'].value);
         //console.log('you submitted value: ', value['maxResults']); 
 
            
